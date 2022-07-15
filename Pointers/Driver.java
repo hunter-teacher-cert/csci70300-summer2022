5402b4fda8e855727752243dec4439029491d10d
Mike Zamansky <zamansky@gmail.com>
2022-07-15 08:50:13 -0400

    Update Driver.java

--- Pointers/Driver.java
+++ Pointers/Driver.java
@@ -36,9 +36,9 @@ public class Driver{
 	// complete the program as per the following comments
 	// and compile and run your program again.
 
-	// Add a line to set a new Node type
-	// variable named walker to the same node as
-	// the variable front points to
+	// Add a line to set the walker variable defined above
+	// so that it points to (refers to) the same node as
+	// the variable front points to.
 	
 	// Uncomment the next line, compile and run
 	// to test what you just added
